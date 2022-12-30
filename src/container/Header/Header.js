8724b90f8064bdf0b469c33191c2d98d78411fb2
shobhit-u <shobhitumrao5@gmail.com>
2022-12-30 17:33:33 +0530

    header css complete

--- src/container/Header/Header.js
+++ src/container/Header/Header.js
@@ -2,7 +2,30 @@ import React from "react";
 import "./Header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header container-center">
+      <div className="logo">Redux Auth</div>
+      <nav className="main-nav">
+        <ul className="main-nav-list">
+          <li>
+            <a className="main-nav-link" href="#EmptyLink">
+              My Products
+            </a>
+          </li>
+          <li>
+            <a className="main-nav-link" href="#EmptyLink">
+              My Sales
+            </a>
+          </li>
+          <li>
+            <a className="btn main-nav-link" href="#EmptyLink">
+              Logout
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
